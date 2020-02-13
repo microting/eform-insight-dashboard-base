3ee86a7b27d3ad7772b38ecef19493ebf81ea296
Leonid <leonid.atf@gmail.com>
2020-02-13 16:26:56 +0200

    Dashboard tests

---
+++ Microting.InsightDashboardBase.Tests/Helpers/DashboardHelpers.cs
@@ -0,0 +1,29 @@
+﻿namespace Microting.InsightDashboardBase.Tests.Helpers
+{
+    using System;
+    using eForm.Infrastructure.Constants;
+    using Infrastructure.Data.Entities;
+
+    public static class DashboardHelpers
+    {
+        public static Dashboard GetNewDashboard()
+        {
+            Random rnd = new Random();
+
+            Dashboard dashboard = new Dashboard
+            {
+                UpdatedByUserId = rnd.Next(1, 255),
+                CreatedByUserId = rnd.Next(1, 255),
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow,
+                WorkflowState = Constants.WorkflowStates.Created,
+                Name = "Name",
+                SurveyId = 1,
+                TagId = 1,
+                LocationId = 1,
+            };
+
+            return dashboard;
+        }
+    }
+}
