1e9acae3460cbd6e394cdc521e952433bb82877a
Alex <sahok6264@gmail.com>
2021-02-11 17:30:34 +0200

    add migration

---
+++ Microting.InsightDashboardBase/Migrations/20210211152628_AddCalculateByWeight.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Microting.InsightDashboardBase.Migrations
+{
+    public partial class AddCalculateByWeight : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CalculateByWeight",
+                table: "DashboardItemVersions",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CalculateByWeight",
+                table: "DashboardItems",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CalculateByWeight",
+                table: "DashboardItemVersions");
+
+            migrationBuilder.DropColumn(
+                name: "CalculateByWeight",
+                table: "DashboardItems");
+        }
+    }
+}
